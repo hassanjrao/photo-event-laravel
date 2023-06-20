a29f2c3ce66c5a9f3145ec527a470a4aad8c3b0c
hassan <hassan@thiqatech.com>
2023-06-20 19:26:14 +0400

    google login

--- config/services.php
+++ config/services.php
@@ -39,7 +39,7 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_SECRET'),
-        'redirect' => 'http://127.0.0.1:7070/callback/google',
+        'redirect' => config("app.url").'/callback/google',
     ],
 
 ];
